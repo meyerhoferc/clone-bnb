0163313955f9f136668fa4113935c58606ebf5c6
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-28 13:43:32 -0600

    Update schema and some models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328193038) do
+ActiveRecord::Schema.define(version: 20170328193708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,13 +45,13 @@ ActiveRecord::Schema.define(version: 20170328193038) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.integer "host_id"
-    t.integer "traveler_id"
     t.date    "start_date"
     t.date    "end_date"
     t.integer "listing_id"
     t.integer "status"
+    t.integer "user_id"
     t.index ["listing_id"], name: "index_reservations_on_listing_id", using: :btree
+    t.index ["user_id"], name: "index_reservations_on_user_id", using: :btree
   end
 
   create_table "roles", force: :cascade do |t|
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20170328193038) do
   add_foreign_key "images", "listings"
   add_foreign_key "listings", "users"
   add_foreign_key "reservations", "listings"
+  add_foreign_key "reservations", "users"
   add_foreign_key "user_roles", "listings"
   add_foreign_key "user_roles", "roles"
   add_foreign_key "user_roles", "users"
