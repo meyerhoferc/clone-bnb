4f0ca4f9766f7459e0a81ea11698818e2fbc386a
James Anderson <james.p.anderson16@gmail.com>
2017-03-29 16:03:29 -0600

    fixed route conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   root "home#index"
   resources :listings, only: [:show, :index]
-  resources :users, only: [:new, :create]
-  get "dashboard", to: "dashboard#show"
-
-=======
   get "dashboard", to: "dashboard#show"
 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   get "logout", to: "sessions#destroy"
 
-  root "home#index"
-
   put "update_host", to: "users#update"
   resources :users, only: [:new, :create, :edit, :update]
->>>>>>> master
+
 end
