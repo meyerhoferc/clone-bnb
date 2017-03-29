b273a0b130279c67882c8798c354ead712d20ccf
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-29 12:59:53 -0600

    Add model validations for user relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 describe User do
-  it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
-  it { should validate_presence_of(:first_name) }
-  it { should validate_presence_of(:last_name) }
-  it { should validate_presence_of(:password) }
-  it { should validate_presence_of(:phone_number) }
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:phone_number) }
+  end
+
+  describe "relationships" do
+    it { should have_many(:listings) }
+    it { should have_many(:reservations) }
+    it { should have_many(:user_roles) }
+    it { should have_many(:roles) }
+  end
+
 end
