c462098907f81f064c548be172ce30b868bfb331
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-29 14:55:44 -0600

    Add feature test for becoming a host

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
   get "dashboard", to: "dashboard#show"
 
   get "login", to: "sessions#new"
