bf5d393eb46ae70cdab588151f4650a8aeaa7cce
Valerie Trudell <v.trudell@protonmail.com>
2017-03-29 16:06:15 -0600

    style changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$(document).ready(function() {
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+});
