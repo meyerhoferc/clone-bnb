702ab21ae1ce5d86d6c4121a410ed15496263924
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-30 10:30:12 -0600

    Add relationship validations for listing

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -13,4 +13,11 @@ describe Listing do
     it { should validate_presence_of(:number_rooms) }
     it { should validate_presence_of(:number_baths) }
   end
+
+  describe "relationships" do
+    it { should belong_to(:user) }
+    it { should have_many(:user_roles) }
+    it { should have_many(:reservations) }
+    it { should have_many(:images) }
+  end
 end
