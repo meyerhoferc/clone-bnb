70607c6b39b283d60877a92208467221480b7f4e
Amara Lovato <amaracuenca@gmail.com>
2017-03-29 21:50:42 -0600

    adds amenities to listings table, improves tests user sees a listing, fixes listings index paragraph ellipsis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328231238) do
+ActiveRecord::Schema.define(version: 20170330005638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,35 @@ ActiveRecord::Schema.define(version: 20170328231238) do
     t.string  "list_category"
     t.string  "number_beds"
     t.string  "number_rooms"
+    t.boolean "number_baths",              default: false
+    t.boolean "elevator",                  default: false
+    t.boolean "pets_allowed",              default: false
+    t.boolean "free_parking",              default: false
+    t.boolean "family_kid_friendly",       default: false
+    t.boolean "doorman",                   default: false
+    t.boolean "pool",                      default: false
+    t.boolean "hot_tub",                   default: false
+    t.boolean "gym",                       default: false
+    t.boolean "air_conditioning",          default: false
+    t.boolean "wheelchair_accessible",     default: false
+    t.boolean "internet",                  default: false
+    t.boolean "smoking_allowed",           default: false
+    t.boolean "suitable_for_events",       default: false
+    t.boolean "wireless_internet",         default: false
+    t.boolean "indoor_fireplace",          default: false
+    t.boolean "breakfast",                 default: false
+    t.boolean "kitchen",                   default: false
+    t.boolean "cable_tv",                  default: false
+    t.boolean "dryer",                     default: false
+    t.boolean "hair_dryer",                default: false
+    t.boolean "washer",                    default: false
+    t.boolean "tv",                        default: false
+    t.boolean "buzzer_wireless_intercom",  default: false
+    t.boolean "iron",                      default: false
+    t.boolean "essentials",                default: false
+    t.boolean "laptop_friendly_workspace", default: false
+    t.boolean "heating",                   default: false
+    t.boolean "private_entrance",          default: false
     t.index ["user_id"], name: "index_listings_on_user_id", using: :btree
   end
 
