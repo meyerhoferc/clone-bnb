6f31570835fc0146d68b18e856ebd354f84057a3
Valerie Trudell <v.trudell@protonmail.com>
2017-03-29 19:55:19 -0600

    adds travel dashboard --profile_page

--- spec/features/users/dashboard/user_can_see_profile_spec.rb
+++ spec/features/users/dashboard/user_can_see_profile_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 describe "user profile" do
   it "traveler profile on user dashboard" do
     traveler = Fabricate(:user)
-    traveler.roles.create(title: "traveler")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:traveler)
+    traveler.roles.create!(title: "traveler")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(traveler)
     visit dashboard_path
     expect(page).to have_content("Profile")
-    expect(page).to have_content("Welcome, #{travel.first_name} #{traveler.last_name}")
+    expect(page).to have_content("Welcome, #{traveler.first_name} #{traveler.last_name}")
     expect(page).to have_content(traveler.about_me)
     expect(page).to have_content(traveler.email)
-    expect(page).to have_content(traveler.phone)
+    expect(page).to have_content(traveler.phone_number)
     expect(page).to have_link("Edit Profile")
 
     expect(page).to have_link("Become a Host")
