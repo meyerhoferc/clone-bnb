c13a2a3437e135aa6cf7ae73ff277077d5a38652
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-03-30 16:14:06 -0600

    Add feature test for checking for double booking before making reservation

--- spec/features/reservations/user_can_create_a_reservation_spec.rb
+++ spec/features/reservations/user_can_create_a_reservation_spec.rb
@@ -42,4 +42,45 @@ describe "a logged in user" do
     expect(listing.range_available?(start_date, end_date)).to eq(false)
     expect(listing.date_available?(middle_date)).to eq(false)
   end
+
+  it "cannot make a reservation for a listing that is booked on that date" do
+
+
+    user = User.create!(email: "email@email.com",
+                        first_name: "Castle",
+                        last_name: "Pines",
+                        about_me: "Boop beep boop",
+                        phone_number: "853-343-2343",
+                        password: "123")
+    user.roles.create!(title: "traveler")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    listing = Fabricate(:listing)
+    image = Fabricate.times(3, :image, listing: listing)
+
+    start_date = "01/01/2018"
+    end_date = "04/01/2018"
+    Reservation.create!(listing: listing,
+                        user: user,
+                        start_date: start_date,
+                        end_date: end_date)
+
+    expect(Reservation.count).to eq(1)
+
+    visit listing_path(listing)
+    click_on "Book Now"
+    expect(current_path).to eq(new_listing_reservation_path(listing))
+
+    fill_in("reservation[start_date]", with: "01/01/2018")
+    fill_in("reservation[end_date]", with: "03/01/2018")
+
+    click_on "Confirm Reservation"
+
+    expect(Reservation.count).to eq(1)
+
+    expect(current_path).to eq(new_reservation_path)
+
+    within(".danger") do
+      expect(page).to have_content("This property is already booked for these days. Please select another date range.")
+    end
+  end
 end
