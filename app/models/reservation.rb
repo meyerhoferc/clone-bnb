bcf00c0faea8a726435569b5ba45ef662a21deb3
James Anderson <james.p.anderson16@gmail.com>
2017-03-30 17:22:49 -0600

    setup reservation confirmation page

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,17 @@ class Reservation < ApplicationRecord
   belongs_to :listing
 
   enum status: [:pending, :confirmed, :complete, :cancelled]
+
+  def num_nights
+    (self.start_date..self.end_date).to_a.count
+  end
+
+  def unit_cost
+    self.listing.cost_per_night
+  end
+
+  def total_cost
+    self.num_nights * self.unit_cost
+  end
+
 end
