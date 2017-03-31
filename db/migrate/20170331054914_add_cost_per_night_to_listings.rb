0a62c36b923a36c108e6268074785f1b8aa8e9be
James Anderson <james.p.anderson16@gmail.com>
2017-03-30 23:54:37 -0600

    reran migration with default price

--- db/migrate/20170331054914_add_cost_per_night_to_listings.rb
+++ db/migrate/20170331054914_add_cost_per_night_to_listings.rb
@@ -1,5 +1,5 @@
 class AddCostPerNightToListings < ActiveRecord::Migration[5.0]
   def change
-    add_column :listings, :cost_per_night, :integer
+    add_column :listings, :cost_per_night, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331050503) do
+ActiveRecord::Schema.define(version: 20170331054914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20170331050503) do
     t.boolean "laptop_friendly_workspace", default: false
     t.boolean "heating",                   default: false
     t.boolean "private_entrance",          default: false
-    t.integer "cost_per_night"
+    t.integer "cost_per_night",            default: 0
     t.index ["user_id"], name: "index_listings_on_user_id", using: :btree
   end
 
