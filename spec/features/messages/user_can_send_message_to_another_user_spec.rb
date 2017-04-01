3865abab4584b342cd9f10ec8f7a501c37af36f4
James Anderson <james.p.anderson16@gmail.com>
2017-04-01 11:17:33 -0600

    added styling for reservation_new, pulled sidebar out into partial

---
+++ spec/features/messages/user_can_send_message_to_another_user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "user can send a message" do
+  context "with proper credentials" do
+    it "when they visit their dashboard" do
+
+    end
+  end
+
+  context "without proper credentials" do
+    it ""
+  end
+end
