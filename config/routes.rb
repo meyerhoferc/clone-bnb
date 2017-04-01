74b2534181eb4a837df897f576e138207b8de1e1
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-01 13:36:59 -0600

    Add feature test for host viewing listings from dashboard sidebar link

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
     resources :messages
     resources :trips
     resources :reservations
-    resources :listings
+    resources :listings, only: [:show, :index]
   end
 end
