4f317c2916cbd7db3fc8949bbaca6637d571e010
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-01 18:13:29 -0600

    Add feature test for traveler viewing trips

---
+++ spec/features/dashboard/user_can_see_trips_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe "a user can see trips" do
+  it "with a role of traveler" do
+    user = User.create!(email: "email@email.com", first_name: "Castle", last_name: "Pines", about_me: "Boop beep boop", phone_number: "853-343-2343", password: "123")
+    user.roles.create!(title: "host")
+    listing = Fabricate(:listing, title: "Super Cool Pad", cost_per_night: 30)
+    start_date = "01/01/2018"
+    end_date = "04/01/2018"
+    Reservation.create!(listing: listing,
+                        user: user,
+                        start_date: start_date,
+                        end_date: end_date)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    within(".sidebar") do
+      click_on "Trips"
+    end
+
+    expect(current_path).to eq(user_trips_path(user))
+
+    within(".my-trips headers") do
+      expect(page).to have_content("Check-In")
+      expect(page).to have_content("Check-Out")
+      expect(page).to have_content("Price")
+      expect(page).to have_content("status")
+    end
+
+    within(".my-trips records") do
+      expect(page).to have_link("Super Cool Pad")
+      expect(page).to have_content("01/01/2018")
+      expect(page).to have_content("04/01/2018")
+      expect(page).to have_content("$90")
+      expect(page).to have_content("Pending")
+    end
+  end
+end
