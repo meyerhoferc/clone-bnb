5963ad28bf631ddc0a368f18a29e7b8194accfec
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-02 11:53:05 -0600

    Add admin dashboard feature test

---
+++ spec/features/admin/user_can_login_as_admin_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "admin dashboard" do
+  it "can be accessed by a logged in admin" do
+    admin = User.create(email: "email@email.com", first_name: "Castle", last_name: "Pines", about_me: "Boop beep boop", phone_number: "853-343-2343", password: "123")
+    admin.roles.create(title: "admin")
+
+    visit root_path
+
+    click_on "Login"
+
+    fill_in("session[email]", with: admin.email)
+    fill_in("session[password]", with: admin.password)
+
+    within(".login_btn") do
+      click_on "Login"
+    end
+
+    expect(current_path).to eq(admin_dashboard_path)
+
+    expect(page).to have_content("Welcome, Castle Pines")
+
+    expect(page).to have_content("Email: email@email.com")
+    expect(page).to have_content("853-343-2343")
+
+    within(".sidebar") do
+      expect(page).to have_link("Profile")
+      expect(page).to have_link("Messages")
+      expect(page).to have_link("Trips")
+      expect(page).to have_link("Reservations")
+      expect(page).to have_link("Listings")
+      expect(page).to have_link("Users")
+    end
+  end
+end
