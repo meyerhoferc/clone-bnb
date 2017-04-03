dda748f26b667a75dae7d2c374bc1b32a61a996c
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-03 13:15:47 -0600

    Update edit profile authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def update
--- spec/features/users/user_can_edit_profile_spec.rb
+++ spec/features/users/user_can_edit_profile_spec.rb
@@ -18,4 +18,19 @@ describe "a logged in user" do
     expect(page).to have_content("new@com.com")
     expect(page).to_not have_content("email@email.com")
   end
+
+  it "cannot edit the profile of another user" do
+    user = User.create(email: "email@email.com", first_name: "User", last_name: "Name", about_me: "Logged in", phone_number: "853-343-2343", password: "123")
+    user.roles.create(title: "traveler")
+    host = User.create(email: "host@email.com", first_name: "Castle", last_name: "Pines", about_me: "Not", phone_number: "853-343-9943", password: "123")
+    host.roles.create(title: "host")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/users/#{host.id}/edit"
+
+    expect(page).to_not have_content("host@email.com")
+    expect(page).to_not have_content("Castle")
+    expect(page).to_not have_content("Pines")
+    expect(page).to_not have_content("Not")
+  end
 end
