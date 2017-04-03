88f99cccae8b8f135dba7b419289a1afad745e54
James Anderson <james.p.anderson16@gmail.com>
2017-04-02 21:04:14 -0600

    added sad path to messages spec

--- spec/features/messages/user_can_send_message_to_another_user_spec.rb
+++ spec/features/messages/user_can_send_message_to_another_user_spec.rb
@@ -32,24 +32,19 @@ describe "traveler can send a message" do
   end
 
   it "they cannot access other people's messages" do
-    listing_1 = Fabricate(:listing)
-    image_1, image_2, image_3 = Fabricate.times(3, :image, listing: listing_1)
+    user_1, user_2 = Fabricate.times(2, :user)
+    user_1.roles.create!(title: "traveler")
+    user_2.roles.create!(title: "traveler")
+    host = Fabricate(:user)
+    host.roles.create!(title: "host")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_2)
 
-    traveler = Fabricate(:user)
-    traveler.roles.create!(title: "traveler")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(traveler)
-    visit "/listings/#{listing_1.id}"
-    click_on "Message #{listing_1.user.first_name}"
+    conversation_1 = Conversation.create!(initiator_id: host.id, recipient_id: user_1.id)
+    message_1 = conversation_1.messages.create!(user_id: user_1.id, body: "msg test 1")
 
-    fill_in("message[body]", with: "Is it available?")
-    click_on "Create Message"
+    visit "/conversations/#{conversation_1.id}/messages"
 
-    within(".card") do
-      expect(page).to have_content("Messages between #{traveler.first_name} and #{listing_1.user.first_name}")
-    end
-    within(".card") do
-      expect(page).to have_content("Is it available?")
-    end
+    expect(page).to have_content("404")
   end
 
 end
